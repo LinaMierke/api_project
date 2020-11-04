d5531f93a43f168731daef1ef35ee2c9074dc2e4
lina mierkE <lina_maria89@hotmail.com>
2020-11-04 12:22:26 -0500

    tested and working

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require("express");
-const app = express ();
-const countries = require("./lib/models/countries")
-const parser = require("body-parser") 
+// const express = require("express");
+// const app = express ();
+// const countries = require("./lib/models/countries")
+// const parser = require("body-parser") 
 
-app.use(parser.json())
+// app.use(parser.json())
 
-app.get("/", (req, res)=>{
-    countries.find({}).then( country =>{
-    res.json()
-    })
-})
+// app.get("/", (req, res)=>{
+//     countries.find({}).then( country =>{
+//     res.json()
+//     })
+// })
 
-app.get("/countries/capital",(req, res)=>{
-    countries.findById(req.params.capital).then(country =>{
-        res.json(country)
-    })
-})
+// app.get("/countries/capital",(req, res)=>{
+//     countries.findById(req.params.capital).then(country =>{
+//         res.json(country)
+//     })
+// })
 
 
-app.listen(4000, ()=> {
-    console.log(" Hi Lina, your app is ON")
-})
+// app.listen(4000, ()=> {
+//     console.log(" Hi Lina, your app is ON")
+// })
+
+const express = require('express')
+const app = express ()
+
+
+app.use(express.json())
+
+
+app.use(require("./lib/routes/Country"))
+
+app.listen(4000, () => console.log('listening on port 4000'))
